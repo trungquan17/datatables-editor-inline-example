8d7d941f4e4ec1a0e182ecc1e8cf7b5c8197725c
trungquan17 <quanlightning17@gmail.com>
2019-07-16 16:44:54 +0700

    done

---
+++ README.md
@@ -0,0 +1,23 @@
+# Datatables with editor inline example
+
+Clone project and run project.
+
+```
+$ git clone https://git_url_clone <project_dir>
+$ cd <project_dir>
+$ npm install
+Run file index.html immediately on your browser
+```
+Demo:
+
+![](images/demo-01.gif)
+
+Author: Trung Quân
+
+Blog: https://trungquandev.com/
+
+CV: https://cv.trungquandev.com/
+
+"A little bit of fragrance always clings to the hands that gives you roses!"
+
+Thanks for watching!
---
+++ custom.js
@@ -0,0 +1,70 @@
+// Init global editor variable
+let editor;
+
+$(document).ready(() => {
+  editor = new $.fn.dataTable.Editor( {
+    table: "#table-example",
+    fields: [ {
+            label: "Name:",
+            name: "name"
+        }, {
+            label: "Position:",
+            name: "position"
+        }, {
+            label: "Office:",
+            name: "office"
+        }, {
+            label: "Age:",
+            name: "age"
+        }, {
+            label: "Start date:",
+            name: "start_date",
+            type: "datetime"
+        }, {
+            label: "Salary:",
+            name: "salary"
+        }
+    ]
+  } );
+
+  $('#table-example').on('click', 'tbody td:not(:first-child)', function (e) {
+    editor.inline(this);
+  });
+
+  editor.on("onSubmitComplete", function(e, json, data, action) {
+    console.log(action);
+    console.log(data);
+    if (action === "edit") {
+      alert(`Bạn vừa edit thành công row có id là ${data.DT_RowId}`);
+    }
+    // do something what you want...
+  });
+
+  $('#table-example').DataTable( {
+    dom: "Bfrtip", // using to show buttons handle data
+    order: [[ 1, 'asc' ]], // asc sort by colum 1
+    columns: [
+      {
+        data: null,
+        defaultContent: '',
+        className: 'select-checkbox',
+        orderable: false
+      },
+      { data: "name" },
+      { data: "position" },
+      { data: "office" },
+      { data: "age" },
+      { data: "start_date" },
+      { data: "salary", render: $.fn.dataTable.render.number( ',', '.', 0, '$' ) }
+    ],
+    select: {
+      style: 'os',
+      selector: 'td:first-child'
+    },
+    buttons: [
+        { extend: "create", editor: editor },
+        { extend: "edit",   editor: editor },
+        { extend: "remove", editor: editor }
+    ]
+  });
+});
